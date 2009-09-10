8f4416449f7e55a916e3dac9dbb54339d46d9565
Ben Brinckerhoff <ben@devver.net>
2009-09-10 11:24:02 -0600

    Changed version numbers for rspec and rspec-rails

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -21,5 +21,5 @@ config.action_controller.allow_forgery_protection    = false
 # ActionMailer::Base.deliveries array.
 config.action_mailer.delivery_method = :test
 
-config.gem 'rspec', :lib => 'spec', :version => "1.2.4"
-config.gem 'rspec-rails', :lib => false, :version => "1.2.4"
+config.gem 'rspec', :lib => 'spec', :version => "1.2.8"
+config.gem 'rspec-rails', :lib => false, :version => "1.2.7.1"
